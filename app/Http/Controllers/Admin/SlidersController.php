3a69e0c8c1e7d8f2d3d8be1308657cf16c5efa35
Elgun837 <elgun.xalilov@bk.ru>
2021-12-16 17:36:22 +0400

    slider-table-connect

--- app/Http/Controllers/Admin/SlidersController.php
+++ app/Http/Controllers/Admin/SlidersController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Slider;
 use Illuminate\Http\Request;
 
 class SlidersController extends Controller
 {
     public function index(){
-        return view('dashboard.sliders.main-slider');
+        $slide = Slider::find(1);
+        return view('dashboard.sliders.main-slider', compact('slide'));
     }
 }
